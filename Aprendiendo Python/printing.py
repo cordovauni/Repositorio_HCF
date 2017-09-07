5d5184a5be445bcf5526826bcbcfa4ac99157263
Miguel Angel Córdova Apolinario <cordovauni@gmail.com>
2017-09-07 15:54:12 -0500

    Primera version de practicando con python

---
+++ Aprendiendo Python/printing.py
@@ -0,0 +1,15 @@
+print("How old are you?")
+age = int(input('what is your name?'))
+# print(age)
+
+if age>18:
+    print('Es mayor de edad')
+else:
+    print('Es menor de eadad')
+
+nombres_mascotas = {'pluto', 'rambo', 'campeon', 'chester', 'ruster'}
+
+for nombre in nombres_mascotas:
+    print(nombre)
+
+
